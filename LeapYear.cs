d0d42c5313b611841b9418075870cbfc02386a80
saurabhs <saurabhpshirsath@gmail.com>
2021-07-23 13:25:34 +0530

    [saurabh] Add ; Checked The Leap Year Or Not

---
+++ LeapYear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCoreProb
+{
+    class LeapYear
+    {
+        public void Leap_Year()
+        {
+            Console.WriteLine("Enter to Year(YYYY) to Check if it is Leap Year or Not: ");
+            int Checkyear = Convert.ToInt32(Console.ReadLine());
+
+            //checking if year is % of 400 or %4 then its a leap year if %100 its not a leap year
+            if ((Checkyear % 400 == 0) || (Checkyear % 4 == 0 && Checkyear % 100 != 0))
+                Console.WriteLine(Checkyear + " is a Leap Year");
+            else
+                Console.WriteLine(Checkyear + " is not a Leap Year");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,28 @@ namespace BasicCoreProb
         {
             Console.WriteLine("Flipped The Coin For Head Or Tail Percentage!");
             Console.WriteLine("1 For: Flip a Coin Head and Tail. ");
+            Console.WriteLine("2 For: Select Value is Leap Year Or not");
+            int selection = Convert.ToInt32(Console.ReadLine());
+
+            switch (selection)
+            {
+                case 1:
+                    //FLIP COIN
+                    FlipCoin FlipCoin = new FlipCoin();
+                    FlipCoin.CheckHeadTailPercentage();
+                    break;
+                case 2:
+                    //CheckLeapYearOrNot
+                    LeapYear LeapYearOrNot = new LeapYear();
+                    LeapYearOrNot.Leap_Year();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection. Select in Between 1 to 2");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
+
+
