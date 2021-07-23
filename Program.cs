f3a421212e98707f6ceedd05e10572477698fd64
saurabhs <saurabhpshirsath@gmail.com>
2021-07-23 12:57:47 +0530

    [saurabh] Add ; Welcome On Basic Prob Displayed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCoreProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Basic Core Problem!");
+        }
+    }
+}
